67db33011eee8585bdff601162eb1ad280be59cb
pholiveira-dev <pedro.alves@escs.edu.br>
2025-07-02 23:08:29 -0300

    Exercícios para revisar os tópicos do curso até aqui

---
+++ aula38/index.js
@@ -0,0 +1,9 @@
+// Exercício: Escreva uma função que recebe 2 números e retorne o maior deles:
+
+function maiorValor(num1, num2){
+
+    return num1 > num2 ? num1 : num2;
+    
+}
+
+console.log(maiorValor(10, 2))
