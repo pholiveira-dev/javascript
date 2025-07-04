e5cf64b46d44fb9af8f03dfc62cf1b3e17b7f576
pholiveira-dev <pedro.alves@escs.edu.br>
2025-07-04 20:26:11 -0300

    exercicio 4

---
+++ aula38/exercicio4.js
@@ -0,0 +1,8 @@
+// Escreva uma função chamada ePaisagem que recebe dois argumentos: largura e altura de uma imagem (number)
+// Retorne true se a imagem estiver no modo paisagem.
+
+const ePaisagem = (largura, altura) => {
+    return largura > altura;
+}
+
+console.log(ePaisagem(1920, 1080));
