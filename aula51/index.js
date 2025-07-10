28f12f27af9b20518d69214a0a3ccb6a6b3f3c3f
pholiveira-dev <pedro.alves@escs.edu.br>
2025-07-09 22:59:47 -0300

    forEach()

---
+++ aula51/index.js
@@ -0,0 +1,15 @@
+let a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+a1.forEach(
+    (valor, indice) => { 
+        console.log(`${indice} : ${valor}`)
+     }
+);
+
+
+
+let frutas = ["maçã", "banana", "laranja"];
+
+frutas.forEach(function(fruta, index) {
+  console.log(`${index}: ${fruta}`);
+});
