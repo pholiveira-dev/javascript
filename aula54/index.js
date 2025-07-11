3c2a0ee69bf6a58cc4627f4cb894d57f3f6be0ac
pholiveira-dev <pedro.alves@escs.edu.br>
2025-07-11 12:58:33 -0300

    Métodos úteis para Objetos

---
+++ aula54/index.js
@@ -0,0 +1,8 @@
+const produto = { nome: 'Produto', preco: 1.8 };
+
+
+//Object.keys (retorna as chaves)
+// Object.freeze (conjela o objeto)
+// Object.assign (def, any - serve pra copiar um objeto)
+// Object.getOwnPropertyDescriptor(o, 'prop')
+// ... spread
