23698d2fc479b9a16443291c0c97dbce86c7143e
pholiveira-dev <pedro.alves@escs.edu.br>
2025-07-14 22:44:03 -0300

    Métodos de instância e estáticos

---
+++ aula58/index.js
@@ -0,0 +1,30 @@
+class ControleRemoto {
+    constructor(tv) {
+        this.tv = tv;
+        this.volume = 0;
+    }
+
+    aumentarVolume() {
+        this.volume += 2;
+    }
+
+    // Método de instância
+    diminuirVolume() {
+        this.volume -= 2;
+    }
+
+    // Método estático
+    static trocaPilha() {
+        console.log('Ok, vou trocar.')
+    }
+}
+
+const controle1 = new ControleRemoto('LG');
+const controle2 = new ControleRemoto('Samsung');
+
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+console.log(controle1);
+
+ControleRemoto.trocaPilha();
